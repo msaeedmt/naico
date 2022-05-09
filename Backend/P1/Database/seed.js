7c5b6c057b2db9e30b8f7f0a8603d1080d4397f6
Saeed <s.motevali96@gmail.com>
2022-05-09 15:14:35 +0430

    some bugs fixed

--- Backend/P1/Database/seed.js
+++ Backend/P1/Database/seed.js
@@ -1,5 +1,6 @@
 const Binance = require('binance-api-node').default
 const mongoose = require('mongoose')
+const moment = require('moment')
 
 const models = require('./models')
 const config = require('../config.json')
@@ -11,20 +12,32 @@ const client = Binance()
 const seedTicker = config.database.seed.ticker
 
 async function fetchData (symbol = seedTicker, interval = '1d', startTime = '2017-01-01') {
-  const ticks = await client.candles({
-    symbol,
-    interval,
-    startTime: new Date(startTime).getTime(),
-    endTime: new Date().getTime()
-  }).then(resp => {
-    const customizeResp = resp.map(element => {
-      element.ticker = symbol
-      return element
-    })
-    return customizeResp
-  })
+  const limit = 1000
+  const currnetTimeMs = new Date().getTime()
+  let allTickes = []
 
-  return ticks
+  do {
+    var nextTwoYearMs = new Date(moment(startTime).add(2, 'year').format('YYYY-MM-DD')).getTime()
+
+    const ticks = await client.candles({
+      symbol,
+      interval,
+      startTime: new Date(startTime).getTime(),
+      endTime: nextTwoYearMs,
+      limit
+    }).then(resp => {
+      const customizeResp = resp.map(element => {
+        element.ticker = symbol
+        return element
+      })
+      return customizeResp
+    })
+
+    allTickes = allTickes.concat(ticks)
+    startTime = moment(startTime).add(2, 'year').format('YYYY-MM-DD')
+  } while (nextTwoYearMs < currnetTimeMs)
+
+  return allTickes
 }
 
 const seedDB = async () => {
